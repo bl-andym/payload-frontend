67677139c240afca7acfbca1d6bf8a2ebf20790f
AndyM <andrew.marsden@bl.uk>
2024-04-09 15:24:17 +0100

    blogposts - simple, can't change folder name, need to edit code to remove default elements

--- src/utils/RenderBlocks.jsx
+++ src/utils/RenderBlocks.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { blocks } from '@/blocks/blockList'
 
 const RenderBlocks = ({ layout }) => (
+    // eslint-disable-next-line no-console
+    console.log('layout: ', layout),
     <div>
         {layout.map((block, i) => {
             const Block = blocks[block.blockType]
